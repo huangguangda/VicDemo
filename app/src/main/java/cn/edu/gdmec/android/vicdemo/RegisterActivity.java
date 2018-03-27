a5afd3be8710e244342a21a53ed9738553f7bf85
huangguangda <2397923107@qq.com>
2018-03-27 16:55:40 +0800

    MD5 加密算法

--- app/src/main/java/cn/edu/gdmec/android/vicdemo/RegisterActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/vicdemo/RegisterActivity.java
@@ -1,7 +1,9 @@
 package cn.edu.gdmec.android.vicdemo;
 
-import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -29,25 +31,36 @@ import android.widget.Toast;
  * 通过SharedPreferences实现数据的存取功能
  * setResult()方法实现界面间的数据回传
  * 通过MD5加密
+ *
+ * 注册界面逻辑代码
+ * 需要获取用户名，用户密码和再次确认密码
+ * 当两次密码相同时，将用户名和密码（经过 MD5 加密）保存到 SharedPreferences 中
+ * 当注册成功之后需要将用户名传递到登录界面 LoginActivity 中
  */
 
-public class RegisterActivity extends Activity implements View.OnClickListener {
-
+public class RegisterActivity extends AppCompatActivity {
     private TextView tv_back;
     private TextView tv_main_title;
     private RelativeLayout title_bar;
+    //用户名、密码、再次输入的密码的控件
     private EditText et_user_name;
     private EditText et_psw;
     private EditText et_psw_again;
     private Button btn_register;
+    //用户名、密码、再次输入的密码的控件的获取值
+    private String userName,psw,pswAgain;
+    //标题布局
+    private RelativeLayout rl_title_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //设置页面布局
         setContentView(R.layout.activity_register);
+        //设置此界面为竖屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         initView();
 
-        findViewById(R.id.btn_register).setOnClickListener(this);
     }
 
     private EditText getEtUserName() {
@@ -62,25 +75,15 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
         return (EditText) findViewById(R.id.et_psw_again);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn_register:
-                //TODO implement
-                break;
-        }
-    }
 
     private void initView() {
+        //从 main_title_bar.xml 页面布局中获得对应的 UI 控件
+        tv_main_title=(TextView) findViewById(R.id.tv_main_title);
+        tv_main_title.setText("注册");
         tv_back = (TextView) findViewById(R.id.tv_back);
-        tv_main_title = (TextView) findViewById(R.id.tv_main_title);
-        title_bar = (RelativeLayout) findViewById(R.id.title_bar);
-        et_user_name = (EditText) findViewById(R.id.et_user_name);
-        et_psw = (EditText) findViewById(R.id.et_psw);
-        et_psw_again = (EditText) findViewById(R.id.et_psw_again);
-        btn_register = (Button) findViewById(R.id.btn_register);
+        rl_title_bar=(RelativeLayout) findViewById(R.id.title_bar);
+        rl_title_bar.setBackgroundColor(Color.TRANSPARENT);
 
-        btn_register.setOnClickListener(this);
     }
 
     private void submit() {
