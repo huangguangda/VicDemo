5b80233ac54c93abe43f87d151a79dbc00cb3c2f
huangguangda <2397923107@qq.com>
2018-03-27 13:00:36 +0800

    添加注册界面

---
+++ app/src/main/java/cn/edu/gdmec/android/vicdemo/RegisterActivity.java
@@ -0,0 +1,25 @@
+package cn.edu.gdmec.android.vicdemo;
+
+/**
+ * Created by Jack on 2018/3/27.
+ * 注册界面主要用于用户输入注册信息，在注册界面中用户需要输入用户名，密码，再次输入密码（确保密码输入无误）
+ * 当点击注册按钮时进行用户注册
+ * 使用本地数据，注册成功后，用户名和密码用SharedPreferences保存
+ * 用MD5进行加密
+ *
+ * 重点：
+ * ImageView，EditText控件，Button控件
+ * SharedPreferences的使用
+ * setResult(RESULT_OK,data)方法的使用
+ * MD5加密
+ *
+ * 学习要点：
+ * 掌握注册界面的设计和逻辑构思
+ * 掌握标题栏的创建和常用控件的使用
+ * 通过SharedPreferences实现数据的存取功能
+ * setResult()方法实现界面间的数据回传
+ * 通过MD5加密
+ */
+
+public class RegisterActivity {
+}
