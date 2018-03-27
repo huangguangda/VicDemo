d55371eb45928f2ccaab6ba0f5f3117e97af81be
huangguangda <2397923107@qq.com>
2018-03-27 11:42:06 +0800

    优化代码块，注解的加入

--- app/src/main/java/cn/edu/gdmec/android/vicdemo/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/vicdemo/SplashActivity.java
@@ -21,17 +21,27 @@ public class SplashActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        //设置此界面为竖屏
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        init();
+
+    }
+
+    private void init() {
         textView = findViewById(R.id.tv_version);
 
         try {
+            //获取程序包信息
             PackageInfo info = getPackageManager().getPackageInfo(getPackageName(),0);
-            textView.setText("version"+info.versionName);
+            textView.setText("version:"+info.versionName);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
+            textView.setText("V");
         }
 
+        //利用 Timer 让此界面延迟 3 秒后再跳转,timer 中有一个线程,这个线程不断执行 timerTask
         Timer timer = new Timer();
+        //timerTask 实现 runnable 接口,TimerTask 类表示一个在指定时间内执行的 timerTask
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
@@ -40,7 +50,7 @@ public class SplashActivity extends AppCompatActivity{
                 SplashActivity.this.finish();
             }
         };
-        timer.schedule(timerTask,3000);
-
+        timer.schedule(timerTask,3000);//设置这个 task 在延迟三秒之后自动执行
     }
+
 }
