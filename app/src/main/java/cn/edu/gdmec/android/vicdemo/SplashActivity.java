7d5c4dcd13e2f9b97c722960d3e31ff372309f4c
huangguangda <2397923107@qq.com>
2018-04-10 15:09:42 +0800

    12

--- app/src/main/java/cn/edu/gdmec/android/vicdemo/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/vicdemo/SplashActivity.java
@@ -15,44 +15,41 @@ import java.util.TimerTask;
  * Created by Jack on 2018/3/27.
  */
 
-public class SplashActivity extends AppCompatActivity{
-    private TextView textView;
+public class SplashActivity extends AppCompatActivity {
+    private TextView tv_version;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        //设置此界面为竖屏
+        //设置此界面为
+        // 竖屏
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         init();
-
     }
-
     private void init() {
-        textView = findViewById(R.id.tv_version);
-
+        tv_version = findViewById(R.id.tv_version);
         try {
             //获取程序包信息
-            //通过PackageManager的getPackageInfo()方法获取PackageInfo对象
-            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(),0);
-            textView.setText("version:"+info.versionName);
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            tv_version.setText("version:" + packageInfo.versionName);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
-            textView.setText("V");
+            tv_version.setText("version");
         }
-
-        //利用 Timer 让此界面延迟 3 秒后再跳转,timer 中有一个线程,这个线程不断执行 timerTask
+        //利用timer让此界面延迟3秒后跳转，timer有一个线程，该线程不断执行task
         Timer timer = new Timer();
-        //timerTask 实现 runnable 接口,TimerTask 类表示一个在指定时间内执行的 timerTask
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                //发送intent实现页面跳转，第一个参数为当前页面的context，第二个参数为要跳转的主页
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
+                //跳转后关闭当前欢迎页面
                 SplashActivity.this.finish();
             }
         };
-        //Timer 的schedule()方法是任务调度方法 调度 TimerTask 执行跳转操作，实现延迟跳转功能
-        timer.schedule(timerTask,3000);//设置这个 task 在延迟三秒之后自动执行
+        //调度执行timerTask，第二个参数传入延迟时间（毫秒）
+        timer.schedule(timerTask, 3000);
     }
-
 }
+
