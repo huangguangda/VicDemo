b3648a92b560c6568816b99b3bbe2ba15843b1d2
huangguangda <2397923107@qq.com>
2018-04-15 02:23:58 +0800

    任务实施ok

---
+++ app/src/main/java/cn/edu/gdmec/android/vicdemo/Bean/UserBean.java
@@ -0,0 +1,14 @@
+package cn.edu.gdmec.android.vicdemo.Bean;
+
+/**
+ * Created by Jack on 2018/4/15.
+ * 需要用一个Bean类来存放这些属性
+ */
+
+public class UserBean {
+    public String userName;//用户名
+    public String nickName;//昵称
+    public String sex;//性别
+    public String signature;//签名
+    public String qq;//qq
+}
